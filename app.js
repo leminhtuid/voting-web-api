01f8b109269fc4af7d30af672c6f1deff6394c3a
Tu Le <leminhtuid@gmail.com>
2017-10-01 18:50:07 +0700

    Delete / in client url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Origin', 'https://ngogiatu-fireprom-vue.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'https://ngogiatu-fireprom-vue.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
